eb5b39a926e6d5b190e1b26b108d8106a5f2c91c
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-08-21 23:04:23 +0800

    Add UI library(Element plus)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import '@/assets/global.css';
 
-import { createApp } from 'vue';
-
 import App from '@/App.vue';
 import router from '@/routes';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createApp } from 'vue';
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(ElementPlus);
 app.mount('#app');
