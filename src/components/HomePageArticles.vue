f0d037050007ea2adf76ae4b1da6285c36f714c5
Phillip XIONG <30094293@tafe.wa.edu.au>
2025-09-19 08:03:02 +0800

    Add Homepage article element

---
+++ src/components/HomePageArticles.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const articleName = '';
+const articleContent = '';
+</script>
+
+<template>
+  <flex-col id="articleFlexCol">
+    <flex-row id="articleFlexRow"> </flex-row>
+  </flex-col>
+</template>
