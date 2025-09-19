792b9f45d95a91017ec4327527e0a797cf47c704
Phillip XIONG <30094293@tafe.wa.edu.au>
2025-09-19 09:23:58 +0800

    Generates ramdom article

---
+++ shared/randomArticle.ts
@@ -0,0 +1,19 @@
+let result = '';
+let title = '';
+const charaters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+const charatersLength = 250;
+const titlelength = 20;
+
+for (let i = 0; i < charatersLength; i++) {
+  result += charaters.charAt(Math.floor(Math.random() * charatersLength));
+}
+for (let i = 0; i < titlelength; i++) {
+  title += charaters.charAt(Math.floor(Math.random() * titlelength));
+}
+
+// export const article= ({
+//   Title =
+// });
+
+// export type User = z.infer<typeof User>;
