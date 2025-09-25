71bb8f587fe052f5854e91c683f501c1bbddcfe0
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-09-25 21:52:38 +0800

    Add basic elements

---
+++ src/components/InfoPopout/WarningPopout.vue
@@ -0,0 +1,17 @@
+<template>
+  <dev class="warning-popout">
+    <h1>{{ message }}</h1>
+  </dev>
+</template>
+<script setup lang="ts">
+const message = 'This is the Warning Popout component.';
+</script>
+<style lang="css">
+.warning-popout {
+  border: 2px solid rgb(255, 255, 0);
+  padding: 1rem;
+  background-color: #ffe6e6;
+  color: #42ad03;
+  border-radius: 8px;
+}
+</style>
