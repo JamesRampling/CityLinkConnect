9c14fda009280d505cb2fbef0e0402752ec64663
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-09-26 00:53:43 +0800

    Change login view and add AnnouncementArticle mobile view

--- src/routes/LoginView.vue
+++ src/routes/LoginView.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>Login!</h1>
+
+  <div class="login-container">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button>Login</button>
+  </div>
 </template>
+<style lang="css">
+#login-container {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+}
+</style>
