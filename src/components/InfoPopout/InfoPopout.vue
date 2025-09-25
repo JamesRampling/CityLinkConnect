ef05dd658cd9be058415a0d7c5aee43e62ed4d54
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-09-25 21:52:38 +0800

    Add basic elements

---
+++ src/components/InfoPopout/InfoPopout.vue
@@ -0,0 +1,17 @@
+<template>
+  <dev class="info-popout">
+    <h1>{{ message }}</h1>
+  </dev>
+</template>
+<script setup lang="ts">
+const message = 'This is the info Popout component.';
+</script>
+<style lang="css">
+.warning-popout {
+  border: 2px solid rgb(34, 0, 255);
+  padding: 1rem;
+  background-color: #ffe6e6;
+  color: #026ad1;
+  border-radius: 8px;
+}
+</style>
---
+++ src/routes/CallEveryThingTestingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AnnouncementArticle
+      title="Exciting Update Released!"
+      author="Jane Doe"
+      date="September 25, 2025"
+      summary="We are thrilled to announce a major update to CityLinkConnect. Enjoy new features and improvements!"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AnnouncementArticle from '@/components/AnnouncementArticle.vue';
+</script>
