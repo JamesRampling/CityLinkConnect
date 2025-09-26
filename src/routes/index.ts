6bc6e3a9f7fdd967f21dafd1c719b5f390f5dd59
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-09-26 09:58:40 +0800

    Add not found page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,10 @@ import AboutView from '@/routes/AboutView.vue';
 import AnnouncementView from '@/routes/AnnouncementView.vue';
 import BookingFormView from '@/routes/BookingFormView.vue';
 import BookingsView from '@/routes/BookingsView.vue';
-import CallEveryThingTestingPage from '@/routes/CallEveryThingTestingPage.vue';
 import FeedbackFormView from '@/routes/FeedbackFormView.vue';
 import HomeView from '@/routes/HomeView.vue';
 import LoginView from '@/routes/LoginView.vue';
+import NotFoundView from '@/routes/NotFoundView.vue';
 import UserProfileView from '@/routes/UserProfileView.vue';
 
 const router = createRouter({
@@ -16,12 +16,12 @@ const router = createRouter({
     { path: '/', component: HomeView },
     { path: '/about', component: AboutView },
     { path: '/bookings', component: BookingsView },
-    { path: '/call-everything-testing', component: CallEveryThingTestingPage },
-    { path: '/booking/:serviceId', component: BookingFormView },
-    { path: '/announcement/:id', component: AnnouncementView },
-    { path: '/user/:id', component: UserProfileView },
+    { path: '/booking/:serviceId', component: BookingFormView, props: true },
+    { path: '/announcement/:id', component: AnnouncementView, props: true },
+    { path: '/user/:id', component: UserProfileView, props: true },
     { path: '/login', component: LoginView },
     { path: '/feedback', component: FeedbackFormView },
+    { path: '/:catchAll(.*)*', component: NotFoundView },
   ],
 });
 
