b32d89baa74e34e1d62045e81f3af8b3d7f8ae31
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-09-26 12:07:16 +0800

    Add input component styles

--- src/routes/BookingFormView.vue
+++ src/routes/BookingFormView.vue
@@ -3,5 +3,28 @@ defineProps<{ serviceId: number }>();
 </script>
 
 <template>
-  <h1>New Service Booking</h1>
+  <div class="page-wrapper">
+    <h1>New Service Booking</h1>
+    <form class="form" action="" @submit.prevent>
+      <div class="input-text">
+        <label for="email-input">Email</label>
+        <input id="email-input" type="text" name="email" />
+      </div>
+
+      <div class="input-text">
+        <label for="date-input">Date</label>
+        <input id="date-input" type="date" name="date" />
+      </div>
+
+      <div class="input-checkbox">
+        <input id="checkbox-input" type="checkbox" name="checkbox-input" />
+        <label for="checkbox-input">Checkbox</label>
+      </div>
+
+      <div class="button-row">
+        <button type="submit" class="button-filled">Submit</button>
+        <button type="reset" class="button-outlined">Reset</button>
+      </div>
+    </form>
+  </div>
 </template>
