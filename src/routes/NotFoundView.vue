34fdb3e029ad77da849d4e6dad4dc7a628b16a7f
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-09-26 09:58:40 +0800

    Add not found page

---
+++ src/routes/NotFoundView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>The page was not found.</h1>
+</template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import BookingsView from '@/routes/BookingsView.vue';
 import FeedbackFormView from '@/routes/FeedbackFormView.vue';
 import HomeView from '@/routes/HomeView.vue';
 import LoginView from '@/routes/LoginView.vue';
+import NotFoundView from '@/routes/NotFoundView.vue';
 import UserProfileView from '@/routes/UserProfileView.vue';
 
 const router = createRouter({
@@ -15,11 +16,12 @@ const router = createRouter({
     { path: '/', component: HomeView },
     { path: '/about', component: AboutView },
     { path: '/bookings', component: BookingsView },
-    { path: '/booking/:serviceId', component: BookingFormView },
-    { path: '/announcement/:id', component: AnnouncementView },
-    { path: '/user/:id', component: UserProfileView },
+    { path: '/booking/:serviceId', component: BookingFormView, props: true },
+    { path: '/announcement/:id', component: AnnouncementView, props: true },
+    { path: '/user/:id', component: UserProfileView, props: true },
     { path: '/login', component: LoginView },
     { path: '/feedback', component: FeedbackFormView },
+    { path: '/:catchAll(.*)*', component: NotFoundView },
   ],
 });
 
