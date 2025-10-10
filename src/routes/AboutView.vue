4c20e812860224316d07946077022a65cd0d965a
SMTAFE-La-JeanHarvey <160809032+SMTAFE-La-JeanHarvey@users.noreply.github.com>
2025-10-10 12:10:48 +0800

    feat: Add content to about page with CityLink Connect information.

--- src/routes/AboutView.vue
+++ src/routes/AboutView.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>About CityLink Connect</h1>
+  <div class="page-wrapper">
+    <h1>About CityLink Connect</h1>
+    <br />
+
+    <h2>What is CityLink Connect?</h2>
+    <p>
+      CityLink Connect is website for all city councils announcements and
+      bookings for any enquiry.
+    </p>
+    <br />
+
+    <h2>Our values</h2>
+    <p>
+      CityLink connect prides itself on the pioneering that takes place and the
+      innovation that we seek to enrich the community we stand with.
+    </p>
+    <p>
+      We want this website to be an accessible domain for everyone regardless of
+      socioeconomic status or disabilities.
+    </p>
+    <p>That is CityLink Connect, connecting each other one person at a time.</p>
+    <br />
+
+    <h2>What can we do using CityLink connect?</h2>
+    <p>Through our platform you can:</p>
+    <ul>
+      <li>
+        Book services such as waste collection, facilty rentals and permits
+      </li>
+      <li>Stay informed through our announcements page.</li>
+      <li>
+        Connect with us via feedback to report any issues with services or
+        website issues.
+      </li>
+    </ul>
+    <br />
+
+    <p>Thank you for making our mission possible.</p>
+  </div>
 </template>
 
 <style scoped></style>
