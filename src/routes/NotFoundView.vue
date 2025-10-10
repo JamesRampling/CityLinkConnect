6d9dc933fa10f0f68a0188e5fe6c3071bb5e7c82
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-10-10 12:25:30 +0800

    Small change on NotFoundView

--- src/routes/NotFoundView.vue
+++ src/routes/NotFoundView.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>The page was not found.</h1>
 </template>
+
+<style lang="css" scoped>
+h1 {
+  text-align: center;
+  color: var(--accent-color);
+}
+
+body {
+  background-color: gray;
+}
+</style>
