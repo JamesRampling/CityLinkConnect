d2d33f6a6561913008806ff8b5aa5e247956e899
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-10-15 22:31:07 +0800

    Fix bookings collection and add test route

--- server/routes/bookings.ts
+++ server/routes/bookings.ts
@@ -1,18 +1,31 @@
-import { bookings } from '#server/database';
+import { Bookings } from '#server/database';
+import { Booking } from '#shared/models';
 import { Router } from 'express';
 
 const bookingsRoute = Router();
 
-bookingsRoute.get('/a', (req, res) => {
-  res.send(5);
-});
-
 bookingsRoute.get('/', (req, res) => {
-  const data = bookings.getAll();
+  const data = Bookings.getAll();
   res.send(data);
 });
 
-// bookingsRoute.post('/', (req, res) => {
-// })
+bookingsRoute.post('/', (req, res) => {
+  const result = Booking.safeParse(req.body);
+  if (!result.success) {
+    res.status(400);
+    res.send(result.error);
+    return;
+  }
+
+  const bookingId = Bookings.insert(result.data);
+  if (bookingId === undefined) {
+    res.status(500);
+    res.send({ error: 'Failed to insert booking' });
+    return;
+  }
+
+  res.status(201);
+  res.send(Bookings.getSingle(bookingId));
+});
 
 export default bookingsRoute;
