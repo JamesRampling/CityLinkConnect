8992c280f67fba987b8e3bd7c0787f3084fee48f
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-10-16 20:03:16 +0800

    Update Not found page

---
+++ src/routes/NotFoundView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <router-link to="/">Go back to Home</router-link>
+  </div>
+</template>
+<style lang="css" scoped>
+.not-found {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+</style>
