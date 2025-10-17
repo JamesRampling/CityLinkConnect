1f3340b77394d65579a21eebebc9b79df44b45fc
James Rampling <30095774@tafe.wa.edu.au>
2025-10-17 12:16:37 +0800

    Fix header not sticking

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,25 @@ const hamburgerMenuExpanded = ref(false);
 <template>
   <header class="">
     <img class="logo-image" src="/favicon.svg" alt="" />
-    <nav>
-      <div
-        class="nav-list"
-        :data-expanded="hamburgerMenuExpanded || undefined"
-        @click="hamburgerMenuExpanded = false"
-      >
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/bookings">Service Bookings</router-link>
-        <router-link to="/feedback">Feedback</router-link>
-      </div>
-      <button
-        class="nav-toggle-button button-outlined"
-        @click="hamburgerMenuExpanded = !hamburgerMenuExpanded"
-      >
-        <IconMenu />
-      </button>
+
+    <button
+      class="nav-toggle-button button-outlined"
+      @click.stop="hamburgerMenuExpanded = !hamburgerMenuExpanded"
+    >
+      <IconMenu />
+    </button>
+
+    <nav
+      class="nav-list"
+      :data-expanded="hamburgerMenuExpanded || undefined"
+      @click="hamburgerMenuExpanded = false"
+    >
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/bookings">Service Bookings</router-link>
+      <router-link to="/feedback">Feedback</router-link>
     </nav>
+
     <div class="end-header-buttons">
       <button
         ref="accessibilityButton"
@@ -69,15 +70,15 @@ const hamburgerMenuExpanded = ref(false);
 
 <style scoped>
 header {
-  /* TODO: Add responsive header (e.g. hamburger) */
-  overflow-x: scroll;
   display: flex;
+  flex-wrap: wrap;
   gap: 1rem;
   align-items: center;
   padding: 0.5rem 1rem;
   border-bottom: 1px solid var(--header-border-color);
 
-  position: relative;
+  position: sticky;
+  top: 0;
   z-index: 1000;
   background: var(--header-bgcolor);
 }
@@ -99,11 +100,6 @@ nav {
   }
 }
 
-.nav-list {
-  display: flex;
-  gap: 1.5rem;
-}
-
 .nav-toggle-button {
   display: none;
 }
@@ -123,13 +119,7 @@ nav {
 
   .nav-list {
     display: none;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    right: 0;
     background: var(--header-bgcolor);
-    border-bottom: 1px solid var(--header-border-color);
-    padding: 1rem;
     z-index: 1000;
 
     flex-direction: column;
@@ -137,6 +127,8 @@ nav {
 
   .nav-list[data-expanded] {
     display: flex;
+    flex-basis: 100%;
+    order: 1;
   }
 
   .nav-menu-backdrop[data-expanded] {
