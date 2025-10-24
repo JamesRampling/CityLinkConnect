49bfef000fbd9eb8950f7f4398b7609cddde0b76
La-Jean Harvey <160809032+SMTAFE-La-JeanHarvey@users.noreply.github.com>
2025-10-24 12:28:01 +0800

    Add hamburger menu dropdown (#5)

---
+++ src/components/icons/IconMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg class="icon" viewBox="0 -960 960 960">
+    <path
+      fill="currentColor"
+      d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+    />
+  </svg>
+</template>
