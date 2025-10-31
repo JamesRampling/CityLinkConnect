ec9c5a8bee4d5805091a16843fe0cbbd95cd6681
PhillipXIONG <30094293@tafe.wa.edu.au>
2025-10-31 10:21:34 +0800

    fix invalid end tag

--- src/components/EditInfo.vue
+++ src/components/EditInfo.vue
@@ -1,10 +1,8 @@
 <template>
-<div id="Popout">
+  <div id="Popout">
     <h1>Please input your changes</h1>
-    <input>Enter your input here</input>
-</div>
+    <input />Enter your input here<input />
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
