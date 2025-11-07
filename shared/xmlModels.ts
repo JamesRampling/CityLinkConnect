e42de630afd6d3b6b5bc4149e431289628cd4bbe
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-11-07 18:24:52 +0800

    Add bi-directional XML text to JS conversion

--- shared/xmlModels.ts
+++ shared/xmlModels.ts
@@ -1,10 +1,45 @@
-import { XMLBuilder, XMLParser } from 'fast-xml-parser';
+import { XMLBuilder, XMLParser, type X2jOptions } from 'fast-xml-parser';
 import z from 'zod';
 
-const parser = new XMLParser({ ignoreAttributes: false });
-const builder = new XMLBuilder({ ignoreAttributes: false });
+const xmlOptions = {
+  ignoreAttributes: false,
+  // Force fees and prices of service to be an array, helps with zod parsing.
+  isArray: (_, jPath) =>
+    ['service.fees.fee', 'service.fees.fee.price'].includes(jPath),
+} satisfies X2jOptions;
 
-export const AnnouncementXML = z.object({
+const parser = new XMLParser(xmlOptions);
+const builder = new XMLBuilder(xmlOptions);
+
+/**
+ * Zod Codec that can parse objects from XML strings, and build XML strings from
+ * objects.
+ * @param schema The schema of the target object that the xml corresponds to.
+ * @returns The XML parser Zod Codec, with the target {@link schema} included.
+ */
+const xml = <T extends z.ZodObject>(schema: T) =>
+  z.codec(z.string(), schema, {
+    decode: (value, ctx) => {
+      try {
+        return parser.parse(value);
+      } catch (e) {
+        ctx.issues.push({
+          code: 'invalid_format',
+          format: 'xml',
+          input: value,
+          message: e instanceof Error ? e.message : undefined,
+        });
+        return z.NEVER;
+      }
+    },
+    encode: (value) => builder.build(value),
+  });
+
+/**
+ * The XML parser output schema for announcements, represents the raw XML file
+ * contents.
+ */
+export const announcementXmlObject = z.object({
   announcement: z.object({
     title: z.string(),
     date: z.iso.date(),
@@ -12,13 +47,20 @@ export const AnnouncementXML = z.object({
   }),
 });
 
-export const AnnouncementJS = z.object({
+/**
+ * The JavaScript object representation of announcements.
+ */
+export const AnnouncementJs = z.object({
   title: z.string(),
   date: z.iso.date(),
   content: z.string(),
 });
 
-export const ServiceXML = z.object({
+/**
+ * The XML parser output schema for services, represents the raw XML file
+ * contents.
+ */
+export const serviceXmlObject = z.object({
   service: z.object({
     name: z.string(),
     description: z.string(),
@@ -29,6 +71,7 @@ export const ServiceXML = z.object({
             price: z
               .object({ '#text': z.string(), '@_variant': z.string() })
               .array(),
+
             '@_title': z.string(),
           })
           .array(),
@@ -37,18 +80,29 @@ export const ServiceXML = z.object({
   }),
 });
 
-export const ServiceJS = z.object({
+/**
+ * The JavaScript object representation of services.
+ */
+export const ServiceJs = z.object({
   name: z.string(),
   description: z.string(),
   fees: z.record(z.string(), z.record(z.string(), z.string())).optional(),
 });
 
-const announcementXMLtoJS = z.codec(AnnouncementXML, AnnouncementJS, {
+/**
+ * Zod Codec that can convert to and from the XML object and JavaScript object
+ * forms of announcements.
+ */
+const announcementXmlToJs = z.codec(announcementXmlObject, AnnouncementJs, {
   decode: (value) => value.announcement,
   encode: (value) => ({ announcement: value }),
 });
 
-const serviceXMLtoJS = z.codec(ServiceXML, ServiceJS, {
+/**
+ * Zod Codec that can convert to and from the XML object and JavaScript object
+ * forms of services.
+ */
+const serviceXmlToJs = z.codec(serviceXmlObject, ServiceJs, {
   decode: (value) => {
     const { name, description, fees: feesObj } = value.service;
 
@@ -90,3 +144,19 @@ const serviceXMLtoJS = z.codec(ServiceXML, ServiceJS, {
     return { service: { name, description, fees: { fee } } };
   },
 });
+
+/**
+ * The contents of services, use {@link ServiceContent.decode} to convert the
+ * XML string to the JavaScript object with the type: {@link ServiceJs}, and use
+ * {@link ServiceContent.encode} to convert the object to an XML string.
+ */
+export const ServiceContent = xml(serviceXmlObject).pipe(serviceXmlToJs);
+
+/**
+ * The contents of services, use {@link AnnouncementContent.decode} to convert the XML
+ * string to the JavaScript object with the type: {@link AnnouncementJs}, and
+ * use {@link AnnouncementContent.encode} to convert the object to an XML string.
+ */
+export const AnnouncementContent = xml(announcementXmlObject).pipe(
+  announcementXmlToJs,
+);
