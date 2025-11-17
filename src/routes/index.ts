0b9c65398146bbeb670f3d304f68ba05cecc6023
FOXTIZEN <hamek01@halotta.com>
2025-11-17 11:43:12 +0800

    Change index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,39 +1,32 @@
-import {
++import {
   createRouter,
   createWebHistory,
   type RouteLocationNormalized,
 } from 'vue-router';
 
-import AboutView from '@/routes/AboutView.vue';
-import AdminPage from '@/routes/AdminPage.vue';
-import AnnouncementView from '@/routes/AnnouncementView.vue';
-import BookingFormView from '@/routes/BookingFormView.vue';
-import FeedbackFormView from '@/routes/FeedbackFormView.vue';
-import HomeView from '@/routes/HomeView.vue';
-import LoginView from '@/routes/LoginView.vue';
-import NotFoundView from '@/routes/NotFoundView.vue';
-import ServiceListView from '@/routes/ServiceListView.vue';
-import UserProfileView from '@/routes/UserProfileView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: HomeView },
-    { path: '/about', component: AboutView },
-    { path: '/services', component: ServiceListView },
+    { path: '/', component: () => import('@/routes/HomeView.vue') },
+    { path: '/about', component: () => import('@/routes/AboutView.vue') },
+    {
+      path: '/services',
+      component: () => import('@/routes/ServiceListView.vue'),
+    },
     {
       path: '/booking/:id',
-      component: BookingFormView,
+      component: () => import('@/routes/BookingFormView.vue'),
       props: convertProps({ id: Number }),
     },
     {
       path: '/announcement/:id',
-      component: AnnouncementView,
+      component: () => import('@/routes/AnnouncementView.vue'),
       props: convertProps({ id: Number }),
     },
     {
       path: '/user/:id',
-      component: UserProfileView,
+      component: () => import('@/routes/UserProfileView.vue'),
       props: convertProps({ id: Number }),
     },
     { path: '/login', component: LoginView },
