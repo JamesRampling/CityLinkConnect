d2fe5759d6ee90f0c3894cafee8043dd80725b4e
James Rampling <30095774@tafe.wa.edu.au>
2025-11-17 15:09:26 +0800

    Save accessibility options to local storage

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,24 @@
+import { Result } from '#shared/utils/Result';
+import z from 'zod';
+
+export function setItem(key: string, item: unknown) {
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+export function getItem<T extends z.ZodType>(
+  key: string,
+  schema: T,
+): Result<z.infer<T>, z.ZodError<T> | 'not-found' | SyntaxError> {
+  try {
+    const itemString = localStorage.getItem(key);
+    if (itemString === null) return Result.err('not-found');
+
+    const json = JSON.parse(itemString);
+    return Result.ok(schema.parse(json));
+  } catch (e) {
+    if (e instanceof z.ZodError || e instanceof SyntaxError) {
+      return Result.err(e);
+    }
+    throw e;
+  }
+}
